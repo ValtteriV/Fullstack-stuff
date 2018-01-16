9a6d57a3213ea444450d53409c5f39e192082fdd
ValtteriV <valtteri.vistiaho@gmail.com>
2018-01-16 19:22:28 +0200

    Anekdoottisivu

--- vko1/src/index.js
+++ vko1/src/index.js
@@ -4,114 +4,81 @@ import ReactDOM from 'react-dom'
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.nimi = 'Half Stack -sovelluskehitys'
-        this.osat = [
-                {
-                    nimi: 'Reactin perusteet',
-                    tehtavia: 10
-                },
-                {
-                    nimi: 'Tiedonvälitys propseilla',
-                    tehtavia: 7
-                },
-                {
-                    nimi: 'Komponenttien tila',
-                    tehtavia: 14
-                }
-            ]
         this.state = {
-            hyvaa: 0,
-            huonoa: 0,
-            neutraalia: 0
+            selected: 0,
+            votes: [0,0,0,0,0,0],
+            biggest: -1
         }
-        this.plus = this.plus.bind(this)
-        this.miinus = this.miinus.bind(this)
-        this.nolla = this.nolla.bind(this)
-        this.ka = 0
-        this.pos = 0
-        this.summa = 0
+        this.nopanheitto = this.nopanheitto.bind(this)
+        this.vote = this.vote.bind(this)
+    }
+
+    nopanheitto() {
+        this.setState({selected: Math.floor((Math.random() * anecdotes.length))})
+    }
+
+    vote() {
+        const temp = this.state.votes.slice()
+        temp[this.state.selected]++
+        if (temp[this.state.selected] > temp[this.state.biggest] || this.state.biggest === -1) {
+            this.setState({biggest: this.state.selected})
+        }
+        this.setState({votes: temp})
     }
 
     render() {
+        const tempanec = this.props.anecdotes.slice()
+        const tempsel = this.state.selected
+        const tempvot = this.state.votes.slice()
+        const tempgirth= this.state.biggest
         return (
             <div>
-                <Otsikko otsikko={'anna palautetta'}/>
-                <Palaute funktio={this.liiallinenFunktio('pos')} nimi={'hyvä'}/>
-                <Palaute funktio={this.liiallinenFunktio('kettu')} nimi={'neutraali'}/>
-                <Palaute funktio={this.liiallinenFunktio('neg')} nimi={'huono'}/>
-                <Otsikko otsikko={'statistiikka'}/>
-                <Sisalto palaute={this}/>
+                <Anecdote anecdotes={tempanec} number={tempsel} votes={tempvot} />
+                <div>
+                    <Button sos={this.vote} name={'vote'}/>
+                    <Button sos={this.nopanheitto} name={'next anecdote'}/>
+                </div>
+                <h1>anecdote with most votes:</h1>
+                <BiggestGirth anecdotes={tempanec} number={tempgirth} votes={tempvot} />
             </div>
+
         )
     }
-
-    liiallinenFunktio(state) {
-        if (state === 'pos')
-            return this.plus
-        if (state === 'neg')
-            return this.miinus
-        return this.nolla
-    }
-
-    plus() {
-        this.setState({hyvaa: this.state.hyvaa + 1})
-    }
-
-    miinus() {
-        this.setState({huonoa: this.state.huonoa + 1})
-    }
-
-    nolla() {
-        this.setState({neutraalia: this.state.neutraalia + 1})
-    }
-
-    avg = () => {
-        return (this.state.hyvaa - this.state.huonoa) / (this.summa)
-    }
-
-    positive = () => { return (this.state.hyvaa / this.summa)*100 + "%"}
 }
 
-const Palaute = (props) => (
-    <button onClick={props.funktio}> {props.nimi} </button>
-)
-
-const Otsikko = (props) => (
-    <h1>{props.otsikko}</h1>
-)
-
-const Sisalto = (props) => {
-    props.palaute.summa = props.palaute.state.hyvaa + props.palaute.state.huonoa + props.palaute.state.neutraalia
-    if (props.palaute.summa === 0) {
-        return (
-        <div>
-            <p>yhtään palautetta ei ole annettu</p>
-        </div>
-        )
-    }
-    props.palaute.ka = props.palaute.avg()
-    props.palaute.pos = props.palaute.positive()
+const Anecdote = (props) => {
     return (
-        <table>
-            <tbody>
-                <Stat osa={props.palaute.state.hyvaa} nimi={'hyvä'}/>
-                <Stat osa={props.palaute.state.neutraalia} nimi={'neutraali'}/>
-                <Stat osa={props.palaute.state.huonoa} nimi={'huono'}/>
-                <Stat osa={props.palaute.ka} nimi={'keskiarvo'}/>
-                <Stat osa={props.palaute.pos} nimi={'positiivisia'}/>
-            </tbody>
-        </table>
+        <div>
+        <p>
+            {props.anecdotes[props.number]}
+        </p>
+            <p> has {props.votes[props.number]} votes</p>
+        </div>
     )
 }
 
-const Stat = (props) => (
-    <tr>
-        <th>{props.nimi}</th>
-        <th>{props.osa}</th>
-    </tr>
+const BiggestGirth = (props) => {
+    if (props.number === -1) return ( <p>nothing has been voted on</p>)
+    return (<Anecdote anecdotes={props.anecdotes} number={props.number} votes={props.votes}/>)
+}
+
+
+const Button = (props) => (
+    <button onClick={props.sos}>{props.name}</button>
 )
 
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+
 ReactDOM.render(
-    <App />,
+    <App anecdotes={anecdotes} />,
     document.getElementById('root')
 )
